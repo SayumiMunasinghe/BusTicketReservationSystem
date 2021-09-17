53bd4f73d7821abdd1424e60ec6d84cdab4b3a01
Sayumi Munasinghe <it20610920@my.sliit.lk>
2021-09-17 18:16:50 +0530

    Created database connection class

---
+++ BusTicketReservationSystem/src/main/java/DBconnection/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package DBconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	protected static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
+		String dbDriver = "com.mysql.jdbc.Driver";
+		String dbURL = "jdbc:mysql:// localhost:3306/";
+		
+		String dbName = "obtrs";
+		String dbUsername = "root";
+		String dbPassword = "password";
+		
+		Class.forName(dbDriver);
+		Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword);
+		
+		return con;
+	}
+}
