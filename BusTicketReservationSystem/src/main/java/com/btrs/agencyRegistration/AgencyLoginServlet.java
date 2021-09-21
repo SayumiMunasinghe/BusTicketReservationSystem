85fc5388ad5ec19e7f1de2cc79da33c77228751c
SenalWeerasekara <Senalweerasekara@gmail.com>
2021-09-21 12:33:28 +0530

    19 session thing added for agency

--- BusTicketReservationSystem/src/main/java/com/btrs/agencyRegistration/AgencyLoginServlet.java
+++ BusTicketReservationSystem/src/main/java/com/btrs/agencyRegistration/AgencyLoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.util.List;
 
 
@@ -37,8 +39,22 @@ public class AgencyLoginServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		
+		//to make session
+		HttpSession session = request.getSession();
+		String mode = "agency";
+		session.setAttribute("mode", mode);
+		
+		//to retrive session
+//		String test = (String)session.getAttribute("mode");
+		
+		//to delete session
+//		session.removeAttribute("mode");
+		
+		
 		RequestDispatcher d4 = request.getRequestDispatcher("viewAgencyDetails.jsp");
 		d4.forward(request, response);
 	}
 
 }
+
+
