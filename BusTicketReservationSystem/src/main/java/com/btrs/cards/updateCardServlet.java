3ea1385cbb2df3aec1bf10353a7f78b4f548e60e
Nabouru <88660439+Nabouru@users.noreply.github.com>
2021-10-02 12:52:43 +0530

    PaymentDetails Retrieve

--- BusTicketReservationSystem/src/main/java/com/btrs/cards/updateCardServlet.java
+++ BusTicketReservationSystem/src/main/java/com/btrs/cards/updateCardServlet.java
@@ -10,36 +10,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class updateCustomerServlet
- */
-@WebServlet("/updateCustomerServlet")
-public class updateCustomerServlet extends HttpServlet {
+
+@WebServlet("/updateCardServlet")
+public class updateCardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = 1;
-		String type = request.getParameter("cardType");
+		if(request.getParameter("update")!=null) { 
+		
+		
 		String cNum = request.getParameter("cno");
 		String name = request.getParameter("hName"); 
 		String cv = request.getParameter("cvv");
 		String expD = request.getParameter("date");
-		
+
 		boolean check;
 		
-		check = cardsDBUtil.updateCard(id,type ,cNum,name,cv,expD );
+		check = cardsDBUtil.updateCard(cNum,name,cv,expD);
 		if(check == true) {
 			 
-			
-		     RequestDispatcher dis = request.getRequestDispatcher("Success.jsp");
+			 
+		     RequestDispatcher dis = request.getRequestDispatcher("ManagePayment.jsp");
 		     dis.forward(request, response);
 		}else {
 			RequestDispatcher dis2 = request.getRequestDispatcher("result2.jsp");
 		     dis2.forward(request, response);
 		}
 	}
-
+	}
 }
 
 
